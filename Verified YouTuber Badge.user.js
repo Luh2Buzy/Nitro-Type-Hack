525bb17ff64bef2a0c3c16fa96ce5fbbbcc97f27
TheRealJ0HN <114599798+TheRealJ0HN@users.noreply.github.com>
2022-09-28 10:24:13 -0500

    Add files via upload

---
+++ Verified YouTuber Badge.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Verified YouTuber Badge
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Enjoy!
+// @author       AlphaTyper
+// @match        https://www.nitrotype.com/garage
+// @grant        none
+// ==/UserScript==
+
+
+
+var _bucket_checker_ = null;
+
+function look_for_bucket(){
+    var a = document.getElementsByClassName('bucket bucket--f');
+    if(a.length != 0 && a.length != undefined){
+        add_the_badge(a);
+        clearInterval(_bucket_checker_);
+    }
+}
+
+function add_the_badge(a){
+    var node = document.createElement("div");
+    node.className = "bucket-media";
+    node.innerHTML = '<img alt="Scoreboard Top 3" class="profile-badge" src="https://cdn.discordapp.com/attachments/740590203042791455/798280755897172059/161039533358529036.png">';
+    a[0].insertBefore(node, a[0].childNodes[0]);
+}
+
+_bucket_checker_ = setInterval(look_for_bucket, 200);
